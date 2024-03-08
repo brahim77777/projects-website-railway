7725037afab5fdbf2c1e82cfa139f7e809980ac8
brahim <brahim@brahims-MacBook-Pro.local>
2024-03-08 22:09:06 +0100

    added validation logic and front-end to the newsletter

---
+++ app/Livewire/SubscribtionCard.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Subscriber;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class SubscribtionCard extends Component
+{
+    public $email;
+    public $done = false;
+
+    public function rules()
+    {
+        return [
+            "email" => [Rule::unique("subscribers"), "required", "email"],
+        ];
+    }
+
+    public function save()
+    {
+        $this->done = false;
+        $this->validate();
+        Subscriber::create(["email" => $this->email]);
+        $this->done = true;
+    }
+    public function remove()
+    {
+        // dd("remove");
+        $this->done = false;
+        $this->resetValidation();
+    }
+    public function render()
+    {
+        return view("livewire.subscribtion-card");
+    }
+}
--- resources/views/components/tailwind-css.blade.php
+++ resources/views/components/tailwind-css.blade.php
@@ -301,29 +301,9 @@
       </div> -->
 
       <!-- Newsletter -->
-      <div class="relative">
-        <div class="absolute left-0 right-0 h-1/2 bg-warm-gray-50" aria-hidden="true"></div>
-        <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-          <div class="rounded-3xl bg-gradient-to-l from-sky-800 to-cyan-700 py-10 px-6 sm:py-16 sm:px-12 lg:flex lg:items-center lg:py-20 lg:px-20">
-            <div class="lg:w-0 lg:flex-1">
-              <h2 class="text-3xl font-bold tracking-tight text-white">Sign up for our newsletter</h2>
-              <p class="mt-4 max-w-3xl text-lg text-cyan-100">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat.</p>
-            </div>
-            <div class="mt-12 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
-              <form class="sm:flex">
-                <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email-address" type="email" autocomplete="email" required="" class="w-full rounded-md border-white px-5 py-3 placeholder-warm-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-cyan-700" placeholder="Enter your email">
-                <button type="submit" class="mt-3 flex w-full items-center justify-center rounded-md border border-transparent bg-green-400 px-5 py-3 text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2 focus:ring-offset-cyan-700 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">Notify me</button>
-              </form>
-              <p class="mt-3 text-sm text-cyan-100">
-                We care about the protection of your data. Read our
-                <!-- space -->
-                <a href="#" class="font-medium text-white underline">Privacy Policy.</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <livewire:subscribtion-card/>
+
+
     </main>
 
     <footer class="bg-white" aria-labelledby="footer-heading">
